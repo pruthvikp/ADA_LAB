8974c83ed2dd5d8aca7d0a1cbbea074af0e77f39
Pruthvi K P <117765336+pruthvikp@users.noreply.github.com>
2023-08-03 12:46:40 +0530

    Update and rename knapsack.c to knapsackcorrect.c

--- 13/knapsackcorrect.c
+++ 13/knapsackcorrect.c
@@ -1,29 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
+#define MAX 20
 
-int a[20][20],opt[20],count=0,i=0,k=0;
+int a[MAX][MAX],opt[MAX],k;
 
 typedef struct table{
     int item,value,weight;
 }T;
 
-int max(int a,int b){
+int max(int a, int b){
     return a>b?a:b;
 }
 
 void knapsack(T *t,int n,int w){
     for(int i=0;i<=n;i++){
         for(int j=0;j<=w;j++){
-            // count++;
             if(i==0 || j==0)
                 a[i][j]=0;
             else{
-                count++;
                 if(j<t[i].weight)
                     a[i][j]=a[i-1][j];
                 else
-                    a[i][j]=max(a[i-1][j], (t[i].value+a[i-1][j-t[i].weight]));
+                    a[i][j]=max(a[i-1][j], (t[i].value + a[i-1][j-t[i].weight]) );              
             }
         }
     }
@@ -43,22 +41,24 @@ void composition(T *t,int n,int w){
 }
 
 void main(){
-    int n=4,w=5;
-    srand(time(NULL));
+    int n=4,w=5,i;
     T *t=malloc(sizeof(T)*(n+1));
+    printf("Enter item's weight and and value\nWt Val\n");
     for(i=1;i<=n;i++){
+        printf("hi");
         t[i].item=i;
-        t[i].weight=(rand()%4)+1;
-        t[i].value=rand()%100;
-        printf("%d\t%d\t%d\n",t[i].item,t[i].weight,t[i].value);
+        printf("hi");
+        scanf("%d",&t[i].weight);
+        scanf("%d",&t[i].value);
+        printf("hi");
     }
+    printf("hi");
     knapsack(t,n,w);
+    printf("hi");
     composition(t,n,w);
-    printf("optimal value= %d\n",a[n][w]);
-    printf("the optimal set:");
+    printf("Optimal value=%d\n",a[n][w]);
+    printf("Optimal set: ");
     for(i=0;i<k;i++)
-        printf(" %d ",opt[i]);
+        printf("%d ",opt[i]);
     printf("\n");
-    printf("Count= %d\n",count);
-
 }
